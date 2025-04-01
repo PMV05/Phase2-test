d41a230d568179026f76a1174690860ef3bde9ca
PMV05 <pedro.vazquez13@upr.edu>
2025-04-01 15:13:14 -0400

    checkout edit WIP

--- frontend_model/checkoutModel.py
+++ frontend_model/checkoutModel.py
@@ -1,19 +1,8 @@
-# TODO:
+        # TODO:
 import pymysql
 from flask import session
 from frontend_model.connectDB import *
 
-
-# # Simulated tuple from customer database
-# # By order: customer ID, first name, last name, address line 1, address line 2, city, state, zip code, email
-# # password, phone number, payment card name, payment card type, payment card number, payment card expiration date, customer status
-
-# user = [1, "Javier", "Quiñones", "Vista Rojas", "Calle 15 L15", "Arecibo", "Puerto Rico", "00612", "javier.quinones3@upr.edu",
-#         "pass1234", 7871231234, "Javier Quiñones", "Mastercard", 1234123412341234, '2023-01-20', "active"]
-
-
-# def getUserModel():
-#     return user
 
 def validateUserModel():
     user = []
@@ -31,23 +20,50 @@ def validateUserModel():
 
     return user
 
+
 def getUserCheckout():
-    # Simulación de obtención de datos del usuario para el checkout
-    return {
-        "phone_number": "1234567890"  # Simula un número de teléfono
-    }
-def editCheckoutQuantity(product_id, new_quantity):
     db = Dbconnect()
-    if new_quantity > 0:
-        query = "UPDATE order_product SET op_product_quantity = %s WHERE product_id = %s AND customer_id = %s"
-        db.execute(query, (new_quantity, product_id, session['customer']))
-        return True  # Successfully updated
+    query = """
+        SELECT
+            c.customer_ID,
+            c.c_first_name,
+            c.c_last_name,
+            a.ad_street,
+            a.ad_city,
+            a.ad_state,
+            a.ad_postal_code,
+            c.c_email,
+            c.c_password,
+            c.c_phone_number
+        FROM customer c
+        LEFT JOIN address a ON c.customer_ID = a.customer_ID
+        LEFT JOIN payment_method pm ON c.customer_ID = pm.customer_ID
+        WHERE c.customer_ID = %s
+    """
+    result = db.select(query, (session['customer'],))
+    if not result:
+        return {}
     
+    row = result[0]
+    user_data = {
+        "customer_ID": row['customer_ID'],
+        "first_name": row['c_first_name'],
+        "last_name": row['c_last_name'],
+        "address_line1": row['ad_street'],
+        "city": row['ad_city'],
+        "state": row['ad_state'],
+        "postal_code": row['ad_postal_code'],
+        "email": row['c_email'],
+        "password": row['c_password'],
+        "phone_number": row['c_phone_number'],
+    }
+    return user_data
+
+      
         
-    
 def saveCartToDB():
     if 'cart' not in session or not session['cart']:
-        return False  # No cart data to save
+        return False 
 
     db = Dbconnect()
 
@@ -55,5 +71,5 @@ def saveCartToDB():
         query = "INSERT INTO order_product (order_id, op_product_id, op_product_quantity, op_product_price) VALUES (%s, %s, %s, %s)"
         db.execute(query, (session['customer'], key, item['quantity'], item['total_price']))
 
-    session.pop('cart')  # Clear session cart after saving
+    session.pop('cart')  
     return True
